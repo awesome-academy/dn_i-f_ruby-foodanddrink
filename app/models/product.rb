e7fe2a5a7654efc14c521d9f43184a26bade1059
Dong Phuoc An <dong.phuoc.an@sun-asterisk.com>
2021-11-28 23:33:52 +0700

    Home list product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_one_attached :images
 
   has_many :order_details, dependent: :destroy
   has_many :orders, through: :order_details
+
+  scope :recent, ->{order updated_at: :DESC}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do |n|
+    name = Faker::Name.name
+    email = "user-#{n}@gmail.com"
+    u = Product.new(
+        name: name,
+        description: email,
+        quantity: rand(2..15),
+        price: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+    )
+    u.images.attach(io: File.open(Rails.root.join("app", "assets", "images", "banh1.jpg")),
+        filename: "banh1.jpg",
+        content_type: "image/jpg")
+    u.save!
+end
