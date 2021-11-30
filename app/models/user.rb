3a01ff873ddc3677fc36ddb578cd664461c2e454
Dong Phuoc An <dong.phuoc.an@sun-asterisk.com>
2021-11-30 09:04:05 +0700

    register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
-<<<<<<< HEAD
+
   has_secure_password
-=======
->>>>>>> 456f310 (login)
+
+  before_save :downcase_email
+
+  validates :name, presence: true, length: {maximum: Settings.length_50}
+  validates :email, uniqueness: true, length: {maximum: Settings.length_50}
+  validates :password, presence: true, length: {minimum: Settings.length_6},
+  allow_nil: true
+
+  private
+  def downcase_email
+    email.downcase!
+  end
 end
