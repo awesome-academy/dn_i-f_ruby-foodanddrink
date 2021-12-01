a862572b074ffa01c4c0c9221b11caa58d7fd9a4
Dong Phuoc An <dong.phuoc.an@sun-asterisk.com>
2021-12-01 09:39:57 +0700

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
+<<<<<<< HEAD
   has_secure_password
+=======
+>>>>>>> 456f310 (login)
 end
